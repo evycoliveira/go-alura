c81bc3098267c4292e48fcd8894a6bb7a08a3b03
Evelyn Cristini Oliveira <evelyn.oliveira@mercadolivre.com>
2022-11-29 12:25:54 -0300

    Add variable inference with typeOf

--- main/main.go
+++ main/main.go
@@ -1,12 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	var nome string = "Evelyn"
-	var idade int = 25
-	var versao float32 = 1.1
+	var nome = "Evelyn"
+	var idade = 25
+	var versao = 1.1
 
 	fmt.Println("Olá, Sra.", nome, "sua idade é", idade)
 	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("O tipo da variavel nome é", reflect.TypeOf(nome))
+	fmt.Println("O tipo da variavel nome é", reflect.TypeOf(idade))
+	fmt.Println("O tipo da variavel nome é", reflect.TypeOf(versao))
 }

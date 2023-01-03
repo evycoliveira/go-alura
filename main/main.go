8183d3fdf27f45988587fc236690d4b1524992c3
Evelyn Cristini Oliveira <evelyn.oliveira@mercadolivre.com>
2023-01-03 10:58:37 -0300

    Add flow control with if else

--- main/main.go
+++ main/main.go
@@ -17,8 +17,16 @@ func main() {
 	fmt.Println("0 - Sair do programa")
 
 	var comando int
-	//fmt.Scan(&comando)
-	fmt.Scanf("%d", &comando)
-	fmt.Println("O endereço da minha variável comando é", &comando)
+	fmt.Scan(&comando)
 	fmt.Println("O comando escolhido foi", comando)
+
+	if comando == 1 {
+		fmt.Println("Monitorando...")
+	} else if comando == 2 {
+		fmt.Println("Exibindo Logs...")
+	} else if comando == 0 {
+		fmt.Println("Saindo do programa")
+	} else {
+		fmt.Println("Não conheço esse comando")
+	}
 }

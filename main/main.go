7d9d63a9ab8a1282c3bce0e93acfb34a7f5caf0b
Evelyn Cristini Oliveira <evelyn.oliveira@mercadolivre.com>
2023-01-04 16:50:11 -0300

    Add repeating for loop, array and status code chack

--- main/main.go
+++ main/main.go
@@ -8,26 +8,25 @@ import (
 
 func main() {
 	exibeIntroducao()
-	exibeMenu()
 
-	nome, idade := devolveNomeEIdade()
-	fmt.Println("Meu nome é:", nome, "e tenho", idade, "anos.")
+	for {
+		exibeMenu()
+		comando := leComando()
 
-	comando := leComando()
-
-	switch comando {
-	case 1:
-		iniciarMonitoramento()
-	case 2:
-		fmt.Println("Exibindo Logs...")
-	case 0:
-		fmt.Println("Saindo do programa")
-		// Indica que o usuário deseja sair com sucesso
-		os.Exit(0)
-	default:
-		fmt.Println("Não conheço esse comando")
-		// Indica que ocorreu uma ação inesperada
-		os.Exit(-1)
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo Logs...")
+		case 0:
+			fmt.Println("Saindo do programa")
+			// Indica que o usuário deseja sair com sucesso
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço esse comando")
+			// Indica que ocorreu uma ação inesperada
+			os.Exit(-1)
+		}
 	}
 }
 
@@ -54,14 +53,20 @@ func leComando() int {
 
 func iniciarMonitoramento() {
 	fmt.Println("Monitorando...")
-	site := "https://www.alura.com.br"
+	var sites [4]string
+	sites[0] = "https://random-status-code.herokuapp.com/"
+	sites[1] = "https://www.alura.com.br/"
+	sites[2] = "https://www.caelum.com.br/"
+
+	fmt.Println(sites)
+
+	site := "https://random-status-code.herokuapp.com/"
 	// Realiza a consulta na URL atribuida a variável
 	resp, _ := http.Get(site)
-	fmt.Println(resp)
-}
 
-func devolveNomeEIdade() (string, int) {
-	nome := "Evelyn"
-	idade := 25
-	return nome, idade
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+	}
 }
